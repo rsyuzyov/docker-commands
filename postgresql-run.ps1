7627a66fea828432eb0c46d93e9b06c38db23c35
docker <docker@Prodline.local>
2017-08-16 19:11:54 +0500

    fix docker registry adrress

--- postgresql-run.ps1
+++ postgresql-run.ps1
@@ -3,4 +3,4 @@ docker volume create --name pg-run
 docker run --name postgresql --restart always `
  -v pg-data:/var/lib/postgresql -v pg-run:/run/postgresql `
  --net host -p 5432:5432 `
- -d rsyuzyov/postgresql-pro-1c
+ -d rsyuzyov/docker-postgresql-pro-1c
